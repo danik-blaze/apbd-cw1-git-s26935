05112185f347eaa75df1b3a288aa181ea651fec4
Tdanik-blaze <s26935@pjwstk.edu.pl>
2026-03-29 17:20:01 +0200

    Implement CalculateAverage method

--- ApbdCw1/StatisticsHelper.cs
+++ ApbdCw1/StatisticsHelper.cs
@@ -2,5 +2,15 @@
 
 public class StatisticsHelper
 {
+public static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        return 0;
+    
+    double sum = 0;
+    foreach (var num in numbers)
+        sum += num;
+    return sum / numbers.Length;
+}
 
 }
