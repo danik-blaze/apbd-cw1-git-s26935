1f036a1501a06ee3afaacc34bd39cacff7993e5a
Tdanik-blaze <s26935@pjwstk.edu.pl>
2026-03-29 17:36:45 +0200

    Add CalculateMax method

--- ApbdCw1/StatisticsHelper.cs
+++ ApbdCw1/StatisticsHelper.cs
@@ -13,4 +13,14 @@ public static double CalculateAverage(int[] numbers)
     return sum / numbers.Length;
 }
 
+public static int CalculateMax(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        throw new ArgumentException("Array cannot be empty");
+    int max = numbers[0];
+    foreach (int num in numbers)
+        if (num > max) max = num;
+    return max;
+}
+
 } //  1231
